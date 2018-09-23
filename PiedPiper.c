1fd5260fb5e6107d94b13ffe063e6ad868eb6f2a
Ryan Filho <ryanfilhoryanfilho@gmail.com>
2018-09-23 19:47:09 -0300

    Renomenado Arquivos

--- PiedPiper.c
+++ PiedPiper.c
@@ -1,46 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <math.h>
-#include <stdbool.h>
-#include "PiedPiper.h"
 
-
-typedef struct no No;
-
-struct no {
-	int frequency;
-	No * left;
-	No * right;
-};
-
-typedef struct arvore Arvore;
-
-struct arvore {
-	No * raiz;
-};	
-
-
-  //////////////////////////
- // Opera��es Gen�ricas  //
-////////////////////////// 
-
-Arvore * criar() {
-	Arvore * arv = (Arvore *) malloc(sizeof(Arvore));
-	arv->raiz = NULL;
-	return arv;
-}
-
-void destruir_rec(No * no) {
-	if (no != NULL) {
-		destruir_rec(no->left);
-		destruir_rec(no->right);
-		free(no);
-	}
-}
-
-void destruir(Arvore * arv) {
-	destruir_rec(arv->raiz);
-	free(arv);
+int main(){
+  printf("Hello World");
+  return 0;
 }
