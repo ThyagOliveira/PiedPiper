9600191f220248d172c55a47a1e514489700d988
Thyago <taothyago@gmail.com>
2018-09-23 19:14:20 -0300

    :sparkles: Add generic list functions

---
+++ List.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "List.h"
+
+typedef struct no No;
+
+struct list {
+	int length;
+	No * start;
+	No * end;
+	
+};
+
+struct no {
+	int info;
+	No * next;
+};
+
+List * create() {
+	List * l = (List *)malloc(sizeof(List));
+	l->start = NULL;
+	l->end = NULL;
+	l->length = 0;
+	return l;
+}
+
+void destroy(List * l) {
+	if (l != NULL) {
+		clear(l);
+		free(l);
+	}
+}
+
+void clear(List * l) {
+	No * aux = l->start;
+	while (aux != NULL) {
+		No * aux2 = aux;
+		aux = aux->prox;
+		free(aux2);
+	}
+	l->start = NULL;
+	l->end = NULL;
+	l->length = 0;
+}
+
+
+int length(List * l)
+{
+	return l->length;
+}
+
+int is_empty(List * l)
+{
+	return length(l) == 0;
+}
---
+++ List.h
@@ -0,0 +1,8 @@
+typedef struct list List;
+
+List * criar();
+void destroy(List * l);
+void clear(List * l);
+int length(List * l);
+int is_empty(List * l);
+
