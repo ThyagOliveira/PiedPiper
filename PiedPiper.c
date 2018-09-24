b2fcbf3560cd3594476d39aa145f2d895edcc522
Thyago <taothyago@gmail.com>
2018-09-24 00:35:36 -0300

    :sparkles::lipstick::beers: Add menu

--- PiedPiper.c
+++ PiedPiper.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <locale.h>
 #include "List.h"
 
 void getByteFrequency(FILE *fileToCompress, unsigned int *listaBytes)
@@ -13,6 +14,7 @@ List * createFrequencyList(FILE *fileToCompress)
   unsigned frequencyList[256] = {0};
   List * byteFrequencyList = create();
   getByteFrequency(fileToCompress, frequencyList);  
+  
   for (int i = 0; i < 256; i++)
   {
       if (frequencyList[i])
@@ -21,7 +23,13 @@ List * createFrequencyList(FILE *fileToCompress)
   return byteFrequencyList;
 }
 
-void main(){
+
+void main() {
+	
+  setlocale(LC_ALL, "");
+  title();
+  		
+  menu();
   printf("Pied Piper\n");
 
   FILE *fileToCompress = fopen("text.txt", "rb");
@@ -34,3 +42,68 @@ void main(){
   
   printList(byteFrequencyList);
 }
+void clean_screen() {
+	system("cls");
+}
+
+void credits() {
+	printf("\n\n\n\n\tAlgoritmo de Huffman desenvolvido em C.\n\tPor Thyago Oliveira e Ryan Filho");
+	printf("\n\n-------------------------------------------------------------------------------\t");
+	printf("\n\tAperte qualquer tecla para voltar ao menu");
+}
+void instructions() {
+	 printf("Uso: huffman [OPCAO] [ARQUIVO] [ARQUIVO]\n\n");
+    printf("Opções:\n");
+    printf("\t-c\tCompactar\n");
+    printf("\t-x\tDescompactar\n");
+    printf("\nExemplo: ./huffman -c teste.txt teste.ed2\n");
+    printf("\n\n-------------------------------------------------------------------------------\t");
+	printf("\n\tAperte qualquer tecla para voltar ao menu");
+}
+void menu() {
+	char choice;
+	
+	printf("\n\n\t\tPied Piper");
+	printf("\n\n\t\t01. Instruções");
+	printf("\n\t\t02. Créditos");
+	printf("\n\t\t03. Sair");
+	printf("\n\n\t\tEscolha a opção: ");
+	scanf("%s", &choice);
+	
+	switch(choice) {
+		case '1':
+			clean_screen();
+			instructions();
+			getch();
+			clean_screen();
+			return menu();
+			break;
+		case '2':
+			clean_screen();
+			credits();
+			getch();
+			clean_screen();
+			return menu();
+			break;
+		case '3':
+			exit(0);
+			break;
+		
+		default:
+			clean_screen();
+			return menu();
+			break;
+	}
+	
+}
+
+void title() {
+
+	system("title Pied Piper");
+	printf("\n\n\t\t\t Projeto de Estrutura de Dados II");
+	printf("\n\t\t\t    Kappa Produções Apresenta");
+	printf("\n\t\t\t\t Pied Piper");
+
+	printf("\n\n-------------------------------------------------------------------------------\t");
+}
+
